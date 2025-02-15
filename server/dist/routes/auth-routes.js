c2d436dd88c58c4756d62c31aa44a4d94be6262e
Caleb Colangelo <calebc041406@gmail.com>
2025-02-15 13:34:06 -0500

    brought over the authenticate token into index.ts

---
+++ server/dist/routes/auth-routes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({
+        where: { username }
+    });
+    if (user) {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (isPasswordValid) {
+            const secretKey = process.env.JWT_SECRET_KEY || '';
+            const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+            return res.json({ token });
+        }
+    }
+    return res.status(401).json({ message: 'Authentication failed' });
+};
+const router = Router();
+router.post('/login', login);
+export default router;
+``;
